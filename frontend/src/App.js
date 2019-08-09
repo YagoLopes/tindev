179d25fbbb21fb63542bb1b6cfc38aa5b68ae86b
Yago Lopes <yagolopeslazaro@gmail.com>
2019-08-09 14:51:22 -0300

    add routes and aplication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Login from "./pages/Login";
 import "./App.css";
+import Routes from "./routes";
 
 function App() {
-  return <Login />;
+  return <Routes />;
 }
 
 export default App;
